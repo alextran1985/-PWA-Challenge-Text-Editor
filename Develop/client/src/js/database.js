5ba02913b0554edd38eec7b3578824637a1f74fe
Alex Tran <Lockey5891@aol.com>
2024-08-27 00:13:04 -0700

    Edited code in database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,13 +14,20 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
-const todosDb = await openDB('todos', 1);
-  const tx = todosDb.transaction('todo', 'readwrite');
-  const store = tx.objectStore('todos');
-  const request = store.put({ id: id, todo: content });
-  const result = await request;
-  console.log('Data saved to the database', result)
+const contentDb = await openDB('content', 1);
+  var tx = contentsDb.transaction('content', 'readwrite');
+  var store = tx.objectStore('contents');
+  var request = store.put({ id: id, content: content });
+  var result = await request;
+  console.log('Data saved to the database', result);
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
-
+console.log('GET all from the database');
+  const contentsDb = await openDB('contents', 1);
+  var tx = contentsDb.transaction('contents', 'readonly');
+  var store = tx.objectStore('contents');
+  var request = store.getAll();
+  var result = await request;
+  console.log('result.value', result);
+  return result;
 initdb();
